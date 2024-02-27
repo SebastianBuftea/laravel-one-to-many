9606ab2a4dd796dacec5892fb47b9b835697d5ee
SebastianBuftea <sebibuftea85@gmail.com>
2024-02-27 15:56:28 +0100

    [UPDATE] seed table type

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+use App\Models\Type;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 25; $i++) {
+            $type = new Type();
+            $type->name = $faker->word();
+            $type->slug = Str::slug($type->name, '-');
+            $type->save();
+        }
+    }
+}
